bcd42aed3127733123f170490c4b2339c72f98db
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-06-27 09:19:28 -0700

    frozen eval check

--- Models/BruteForce/run_single_node.sh
+++ Models/BruteForce/run_single_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=p_ccib_1
-#SBATCH --job-name=eval_U_a,theta_3reps
+#SBATCH --job-name=eval_F_a,theta_3reps
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:2 # Number of GPUs
 #SBATCH --constraint=volta
 #SBATCH --time=48:00:00
-#SBATCH --output=slurm_log/slurm.%N.eval_U_a,theta_3reps.out
-#SBATCH --error=slurm_log/slurm.%N.eval_U_a,theta_3reps.err
+#SBATCH --output=slurm_log/slurm.%N.eval_F_a,theta_3reps.out
+#SBATCH --error=slurm_log/slurm.%N.eval_F_a,theta_3reps.err
 #SBATCH --export=ALL
 
 pwd
