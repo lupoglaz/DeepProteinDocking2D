bdef4ac5850fae7ede619df5aa42a1043aadc447
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-07-03 18:29:52 -0700

    test: bias, unfrozen

--- Models/BruteForce/run_single_node.sh
+++ Models/BruteForce/run_single_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=p_ccib_1
-#SBATCH --job-name=bias_scratch_1rep
+#SBATCH --job-name=bias_unfrozen_1rep
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:2 # Number of GPUs
 #SBATCH --constraint=volta
 #SBATCH --time=48:00:00
-#SBATCH --output=slurm_log/slurm.%N.bias_scratch_1rep.out
-#SBATCH --error=slurm_log/slurm.%N.bias_scratch_1rep.err
+#SBATCH --output=slurm_log/slurm.%N.bias_unfrozen_1rep.out
+#SBATCH --error=slurm_log/slurm.%N.bias_unfrozen_1rep.err
 #SBATCH --export=ALL
 
 pwd
