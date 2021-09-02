1f08f96973a639e6c1f529909cd0073fe094f8a6
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-02 13:38:44 -0400

    alphafold change output dir

--- Models/BruteForce/alphafold_gpu.sh
+++ Models/BruteForce/alphafold_gpu.sh
@@ -31,6 +31,6 @@ singularity run -B $ALPHAFOLD_DATA_PATH:/data -B .:/etc --pwd /app/alphafold --n
     --fasta_paths=/scratch/pgarias/fastafiles/sarscovid2.fasta \
 #    --output_dir=/scratch/pgarias/outputdir \
 #    --output_dir=/home/sb1638/alphafold_test \
-    --output_dir=/scratch/sb1638/af2_output \
+#    --output_dir=/scratch/sb1638/af2_output \
     --model_names=model_1,model_2,model_3,model_4,model_5 \
     --max_template_date=2020-01-01
