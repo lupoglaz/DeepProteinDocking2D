8338054deff707e94517ae77634b8f66756dbb3a
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-03 14:07:47 -0400

    alphafold2 p_ccib_1 whole node mem

--- Models/BruteForce/alphafold_gpu.sh
+++ Models/BruteForce/alphafold_gpu.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu              # Partition (job queue)
+##SBATCH --partition=gpu              # Partition (job queue)
+#SBATCH --partition=p_ccib_1
 #SBATCH --requeue                    # Return job to the queue if preempted
 #SBATCH --job-name=alphafold         # Assign an short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=8                   # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
 #SBATCH --gres=gpu:1                 # Number of GPUs
-#SBATCH --mem=32000                  # Real memory (RAM) required (MB), 0 is the whole-node memory
-#SBATCH --time=03:00:00              # Total run time limit (HH:MM:SS)
+##SBATCH --mem=32000                  # Real memory (RAM) required (MB), 0 is the whole-node memory
+#SBATCH --mem=0
+#SBATCH --time=08:00:00              # Total run time limit (HH:MM:SS)
 #SBATCH --output=slurm.%N.%j.out     # STDOUT output file
 #SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
 #SBATCH --export=ALL                 # Export you current env to the job env
@@ -31,4 +33,7 @@ singularity run -B $ALPHAFOLD_DATA_PATH:/data -B .:/etc --pwd /app/alphafold --n
     --fasta_paths=/scratch/sb1638/fastafiles/spike_sarscovid2.fasta \
     --output_dir=/scratch/sb1638/af2_output \
     --model_names=model_1,model_2,model_3,model_4,model_5 \
-    --max_template_date=2020-01-01
+    --max_template_date=2020-05-14
+
+
+#    --max_template_date=2020-01-01
