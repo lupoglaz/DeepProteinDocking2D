c5f8d680b0c2d511d819de2890fe440b0506dff7
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-13 17:34:05 -0400

    FI: 2ep expB -logsumexp to sigmoid(-x)

--- Models/BruteForce/run_single_node_FI.sh
+++ Models/BruteForce/run_single_node_FI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=p_ccib_1
-#SBATCH --job-name=2ep_expC_nlse_sig
+#SBATCH --job-name=2ep_expB_nlse_sig
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:2 # Number of GPUs
 #SBATCH --constraint=volta
 #SBATCH --time=48:00:00
-#SBATCH --output=slurm_log/slurm.%N.2ep_expC_nlse_sig.out
-#SBATCH --error=slurm_log/slurm.%N.2ep_expC_nlse_sig.err
+#SBATCH --output=slurm_log/slurm.%N.2ep_expB_nlse_sig.out
+#SBATCH --error=slurm_log/slurm.%N.2ep_expB_nlse_sig.err
 #SBATCH --export=ALL
 
 pwd
