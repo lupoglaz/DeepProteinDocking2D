be2318c8c346efdb958b6511edec2439401378ff
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-13 22:31:57 -0400

    FI: 2ep expB -logsumexp to sigmoid(-x)

--- Models/BruteForce/train_bruteforce_interaction.py
+++ Models/BruteForce/train_bruteforce_interaction.py
@@ -44,8 +44,8 @@ class BruteForceInteractionTrainer:
     # testcase = '2ep_expC_nlse_nsig'
     # testcase = '2ep_expB_nlse_nsig'
 
-    testcase = '2ep_expC_nlse_sig_nx'
-    # testcase = '2ep_expB_nlse_sig_nx'
+    # testcase = '2ep_expC_nlse_sig_nx'
+    testcase = '2ep_expB_nlse_sig_nx'
 
 
     ###################### Load and freeze/unfreeze params (training no eval)
@@ -54,8 +54,8 @@ class BruteForceInteractionTrainer:
     pretrain_model.load_state_dict(torch.load(path_pretrain)['state_dict'])
 
     # param_to_freeze = 'all'
-    param_to_freeze = 'W' ##freeze all but "a" weights
-    # param_to_freeze = None
+    # param_to_freeze = 'W' ##freeze all but "a" weights
+    param_to_freeze = None
 
     ## for exp d
     #### load (pretrained: IP CNN frozen, a00...a11 unfrozen) and retrain IP as unfrozen (d exp)
