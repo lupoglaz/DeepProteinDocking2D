fc212ef2d8b8d2992e8b5b0a131288b66971e506
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-25 20:14:37 -0400

    FI expA final

--- Models/BruteForce/run_single_node_FI.sh
+++ Models/BruteForce/run_single_node_FI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=p_ccib_1
-#SBATCH --job-name=scratch_final
+#SBATCH --job-name=expA_final
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:2 # Number of GPUs
 #SBATCH --constraint=volta
 #SBATCH --time=48:00:00
-#SBATCH --output=slurm_log/slurm.%N.scratch_final.out
-#SBATCH --error=slurm_log/slurm.%N.scratch_final.err
+#SBATCH --output=slurm_log/slurm.%N.expA_final.out
+#SBATCH --error=slurm_log/slurm.%N.expA_final.err
 #SBATCH --export=ALL
 
 pwd
