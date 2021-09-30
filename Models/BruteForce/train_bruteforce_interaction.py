dc9b77a44944ce6dc62d8dd41494ae1a99cb8214
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-30 12:53:50 -0400

    expC lr-4 ones init final

--- Models/BruteForce/train_bruteforce_interaction.py
+++ Models/BruteForce/train_bruteforce_interaction.py
@@ -47,8 +47,8 @@ class BruteForceInteractionTrainer:
     case = 'final_lr4_ones'
 
     # exp = 'A'
-    exp = 'B'
-    # exp = 'C'
+    # exp = 'B'
+    exp = 'C'
     # exp = 'scratch'
 
     testcase = 'exp' + exp + '_' + case
