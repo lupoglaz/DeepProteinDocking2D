f733b6b2d78f88bcf84e040e81dd34e4f2f1f917
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-10-09 14:29:11 -0400

    FI 1s4v scratch

--- Models/BruteForce/run_single_node_FI.sh
+++ Models/BruteForce/run_single_node_FI.sh
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:2 # Number of GPUs
 #SBATCH --constraint=volta
 #SBATCH --time=48:00:00
-#SBATCH --output=slurm_log/slurm.%N.1s4v_scratch.out
-#SBATCH --error=slurm_log/slurm.%N.1s4v_scratch.err
+#SBATCH --output=slurm_log/slurm.%N.%j.out
+#SBATCH --error=slurm_log/slurm.%N.%j.err
 #SBATCH --export=ALL
 
 pwd
