b7f7dbe6160ae910253f6ba40402779896422590
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-10-26 20:29:18 -0400

    run ebm on amarel

---
+++ run_ebm_IP.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=p_ccib_1
+#SBATCH --job-name=EBM_IP_1LDsamp_100ep_GS
+#SBATCH --nodes=1
+##SBATCH --ntasks=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:2 # Number of GPUs
+#SBATCH --constraint=volta
+#SBATCH --time=48:00:00
+#SBATCH --output=slurm_log/slurm.%N.%j.%x.out
+#SBATCH --error=slurm_log/slurm.%N.%j.%x.err
+#SBATCH --export=ALL
+
+pwd
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+exp=$(echo EBM_IP_1LDsamp_100ep_GS)
+
+#EBM: 1D sample buffer, no add_positive
+python train_docking.py -experiment "$exp" -train -ebm -num_epochs 100 -batch_size 1 -gpu 0 -num_samples 1 -no_pos_samples
+python results.py -experiment "$exp" -docking -max_epoch 100
+python train_docking.py -experiment "$exp" -test -ebm -gpu 0
