777bd65ff7e3e226ff2c9df000486e47f628cb6d
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-06 14:25:32 -0400

    updated to match shapes of MC and BF sampling free E

--- Models/BruteForce/model_bruteforce_interaction.py
+++ Models/BruteForce/model_bruteforce_interaction.py
@@ -7,21 +7,24 @@ class BruteForceInteraction(nn.Module):
         super(BruteForceInteraction, self).__init__()
         # self.debug = False
         self.F_0 = nn.Parameter(torch.zeros(1, requires_grad=True))
-        self.logdimsq = torch.log(torch.tensor(100 ** 2))
+        self.volume = torch.log(torch.tensor(100 ** 2))
 
     def forward(self, FFT_score, plotting=False, debug=False):
-        E = -FFT_score
-        if len(E.shape) > 3:
-            E = E.squeeze()
+        ##TODO: pass BETA
+
+        E = -FFT_score.squeeze()
         if len(E.shape) < 3:
             E = E.unsqueeze(0)
         if E.shape[0] > 1:
-            # print(E.shape[0])
-            self.logdimsq = torch.log(E.shape[0]*torch.tensor(100 ** 2))
+            self.volume = torch.log(E.shape[0]*torch.tensor(100 ** 2))
         # print(self.logdimsq)
 
-        # F = -torch.logsumexp(-E, dim=(0, 1, 2))
-        F = -(torch.logsumexp(-E, dim=(0, 1, 2)) - self.logdimsq)
+        if E.shape[0] == 360:
+            F = -(torch.logsumexp(-E, dim=(0, 1, 2)) - self.volume)
+        else:
+            translationsF = torch.logsumexp(-E, dim=(1, 2))
+            F = -(translationsF - self.volume)
+            F = torch.mean(F, dim=0)
         deltaF = F - self.F_0
         pred_interact = torch.sigmoid(-deltaF)
 
