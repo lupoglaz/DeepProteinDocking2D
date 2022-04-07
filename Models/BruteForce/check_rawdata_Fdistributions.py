c7005c0bc66967d21600d645abef208182a9bb50
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-06 22:14:23 -0400

    checked raw dataset features F distributions, not cleanly separable

--- Models/BruteForce/check_rawdata_Fdistributions.py
+++ Models/BruteForce/check_rawdata_Fdistributions.py
@@ -43,15 +43,11 @@ if __name__ == "__main__":
                                     # weight_bound=3.0, weight_crossterm1=-0.3, weight_crossterm2=-0.3, weight_bulk=2.8)
                                     weight_bound = 3.0, weight_crossterm1 = -0.3, weight_crossterm2 = -0.3, weight_bulk = 30)
 
-        # E = -FFT_score
-        # F = -(torch.logsumexp(-E, dim=(0, 1, 2)) - volume)
-
-        E = FFT_score
+        E = -FFT_score
         F = -(torch.logsumexp(-E, dim=(0, 1, 2)) - volume)
 
         with open(filename, 'a') as fout:
             fout.write('%f\t%f\t%d\n' % (F.item(), F.item(), gt_interact.item()))
 
     # FILossPlotter('ManuscriptWeights').plot_deltaF_distribution(filename=filename, binwidth=100, show=True)
-    # FILossPlotter('AdjustedHighBulkWeight=30').plot_deltaF_distribution(filename=filename, binwidth=100, show=True)
-    FILossPlotter('MinimizeE_AdjustedHighBulkWeight=30').plot_deltaF_distribution(filename=filename, binwidth=100, show=True)
+    FILossPlotter('AdjustedHighBulkWeight=30').plot_deltaF_distribution(filename=filename, binwidth=100, show=True)
