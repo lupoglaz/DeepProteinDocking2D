fb417352e4d261abd17a83b39314db5a4884ce40
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-23 18:43:05 -0400

    new datasets testing, updating data loader

--- DatasetGeneration/generate_dataset.py
+++ DatasetGeneration/generate_dataset.py
@@ -1,14 +1,16 @@
 import _pickle as pkl
 from pathlib import Path
-import random
 import argparse
 import sys
+import random
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
 from os.path import exists
 from DeepProteinDocking2D.DatasetGeneration.ProteinPool import ProteinPool, ParamDistribution
 from DeepProteinDocking2D.Utility.torchDockingFFT import TorchDockingFFT
-from DeepProteinDocking2D.Utility.utility_functions import read_pkl, write_pkl, plot_assembly
+from DeepProteinDocking2D.Utility.utility_functions import Utility
 
 def generate_shapes(params, savefile, num_proteins=500):
     pool = ProteinPool.generate(num_proteins=num_proteins, params=params)
@@ -21,106 +23,25 @@ def generate_interactions(docker, savefile):
     pool.save(savefile)
 
 
-# def extract_docking_dataset(docker, interaction_criteria, savefile,
-#                             output_files=['DatasetGeneration/docking_data_train_torchv1p10.pkl',
-#                                           'DatasetGeneration/docking_data_valid_torchv1p10.pkl'],
-#                             max_num_samples=1100):
-#     pool = ProteinPool.load(savefile)
-#     dataset_all = pool.extract_docking_dataset(docker, interaction_criteria, max_num_samples=max_num_samples)
-#     total_len = len(dataset_all)
-#     print(f'Total data length {total_len}')
-#     if len(output_files) == 2:
-#         train_len = int(0.8 * total_len)
-#         valid_len = total_len - train_len
-#         print(f'Training/validation split length {train_len} / {valid_len}')
-#         random.shuffle(dataset_all)
-#         with open(output_files[0], 'wb') as fout:
-#             pkl.dump(dataset_all[:train_len], fout)
-#         with open(output_files[1], 'wb') as fout:
-#             pkl.dump(dataset_all[train_len:], fout)
-#     elif len(output_files) == 1:
-#         with open(output_files[0], 'wb') as fout:
-#             pkl.dump(dataset_all, fout)
-#
-#
-# def extract_interaction_dataset(interaction_criteria, savefile,
-#                                 output_files=['DatasetGeneration/interaction_data_train_torchv1p10.pkl',
-#                                               'DatasetGeneration/interaction_data_valid_torchv1p10.pkl'],
-#                                 num_proteins=100):
-#     pool = ProteinPool.load(savefile)
-#     if len(output_files) == 2:
-#         train_len = int(0.8 * num_proteins)
-#         valid_len = num_proteins - train_len
-#         print(f'Training/validation split length {train_len} / {valid_len}')
-#         interactome_train = pool.extract_interactome_dataset(interaction_criteria, ind_range=(0, train_len))
-#         interactome_valid = pool.extract_interactome_dataset(interaction_criteria, ind_range=(train_len, num_proteins))
-#         with open(output_files[0], 'wb') as fout:
-#             pkl.dump(interactome_train, fout)
-#         with open(output_files[1], 'wb') as fout:
-#             pkl.dump(interactome_valid, fout)
-#     elif len(output_files) == 1:
-#         interactome_test = pool.extract_interactome_dataset(interaction_criteria, ind_range=(0, num_proteins))
-#         with open(output_files[0], 'wb') as fout:
-#             pkl.dump(interactome_test, fout)
-#
-#
-# def visualize(docker, savefile):
-#     pool = ProteinPool.load(savefile)
-#     pool.plot_interaction_dist(perc=90)
-#     # pool.plot_interactions(docker, filename='examples.png', num_plots=10)
-#     pool.plot_sample_funnels(docker, filename='funnels.png', range=[(-200, -150), (-200, -100), (-100, -20)])
-#     pool.plot_params()
-#
-#
-# def generate_set(num_proteins, params, interaction_criteria_dock, interaction_criteria_inter,
-#                  docker, savefile, training_set=True, num_docking_samples=1100, num_inter_proteins=100):
-#     pass
-
-
-# Structures
-# if exists(savefile):
-# 	print(savefile, 'already exists!')
-# 	print('this protein shape pool will be loaded for dataset generation..')
-# else:
-# 	print(savefile, 'does not exist')
-# 	print('generating pool of', str(num_proteins), 'protein shapes...')
-# 	generate_shapes(params, savefile, num_proteins)
-# 	#Interaction
-# 	generate_interactions(docker, savefile)
-
-# if training_set:
-# 	#Docking dataset
-# 	extract_docking_dataset(docker, interaction_criteria_dock, savefile,
-# 							output_files=['DatasetGeneration/docking_data_train_torchv1p10.pkl', 'DatasetGeneration/docking_data_valid_torchv1p10.pkl'],
-# 							max_num_samples = num_docking_samples)
-# 	# #Interaction dataset
-# 	extract_interaction_dataset(interaction_criteria_inter, savefile,
-# 							output_files=['DatasetGeneration/interaction_data_train_torchv1p10.pkl', 'DatasetGeneration/interaction_data_valid_torchv1p10.pkl'],
-# 							num_proteins = num_inter_proteins)
-# else:
-# 	#Docking dataset
-# 	extract_docking_dataset(docker, interaction_criteria_dock, savefile,
-# 							output_files=['DatasetGeneration/docking_data_test_torchv1p10.pkl'],
-# 							max_num_samples = num_docking_samples)
-# 	#Interaction dataset
-# 	extract_interaction_dataset(interaction_criteria_inter, savefile,
-# 							output_files=['DatasetGeneration/interaction_data_test_torchv1p10.pkl'],
-# 							num_proteins = num_inter_proteins)
-
-# def find_best_pose(receptor, ligand):
-#     receptor, ligand = protein_shapes[i], protein_shapes[j]
-#     receptor = torch.tensor(receptor, dtype=torch.float).cuda()
-#     ligand = torch.tensor(ligand, dtype=torch.float).cuda()
-#     receptor_stack = FFT.make_boundary(receptor)
-#     ligand_stack = FFT.make_boundary(ligand)
-#     fft_score = FFT.dock_global(receptor_stack, ligand_stack)
-#     rot, trans = FFT.extract_transform(fft_score)
-#     best_score = fft_score[rot, trans[0], trans[1]]
-#     return best_score
-
 if __name__ == '__main__':
-    protein_pool = 'protein_pool.pkl'
-    num_proteins = 100
+
+    random_seed = 42
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    random.seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.cuda.set_device(0)
+    # torch.set_printoptions(precision=3)
+
+
+    swap_quadrants = False
+    # swap_quadrants = True
+    FFT = TorchDockingFFT(swap_plot_quadrants=swap_quadrants, normalization='ortho')
+    Utility = Utility()
+    plotting = True
+    num_proteins = 10
+    protein_pool = 'protein_pool'+str(num_proteins)+'.pkl'
     params = ParamDistribution(
         alpha=[(0.8, 1), (0.9, 9), (0.95, 4)],
         num_points=[(20, 1), (30, 2), (50, 4), (80, 8), (100, 6)]
@@ -133,19 +54,27 @@ if __name__ == '__main__':
         print('generating pool of', str(num_proteins), 'protein shapes...')
         generate_shapes(params, protein_pool, num_proteins)
 
-    # # Interaction
-    # generate_interactions(docker, protein_pool)
 
-    swap_quadrants = True
-    FFT = TorchDockingFFT(swap_plot_quadrants=swap_quadrants)
 
-    data = read_pkl('protein_pool')
+    data = Utility.read_pkl(protein_pool[:-4])
     protein_shapes = data[0]
 
-    FFT_score_list = []
-    weight_bound, weight_crossterm1, weight_crossterm2, weight_bulk = 1, 2, 2, -20
-    for i in range(2):
-        for j in range(2):
+    weight_bound, weight_crossterm1, weight_crossterm2, weight_bulk = 10, 20, 20, -200
+    # weight_bound, weight_crossterm1, weight_crossterm2, weight_bulk = 1, 2, 2, -20
+    docking_score_threshold = 50
+    docking_set_length = 50
+    interaction_score_threshold = 0
+    interaction_set_length = 100
+    FFT_score_list = [[], []]
+    docking_set = []
+    interaction_set = [[],[]]
+    plot_count = 0
+
+    interaction_matrix = torch.zeros(num_proteins, num_proteins)
+
+    for i in tqdm(range(num_proteins)):
+        for j in tqdm(range(num_proteins)):
+            plot_count += 1
             receptor, ligand = protein_shapes[i], protein_shapes[j]
             receptor = torch.tensor(receptor, dtype=torch.float).cuda()
             ligand = torch.tensor(ligand, dtype=torch.float).cuda()
@@ -154,92 +83,45 @@ if __name__ == '__main__':
             fft_score = FFT.dock_global(receptor_stack, ligand_stack, weight_bound, weight_crossterm1, weight_crossterm2, weight_bulk)
             rot, trans = FFT.extract_transform(fft_score)
             best_score = fft_score[rot.long(), trans[0].long(), trans[1].long()]
-            pair = plot_assembly(receptor.cpu(), ligand.cpu(), rot.cpu(), trans.cpu())
-            plt.imshow(pair.transpose())
-            plt.show()
-            FFT_score_list.append([i, j, best_score.item()])
+
+            if best_score > docking_score_threshold and len(docking_set) < docking_set_length:
+                docking_set.append([receptor, ligand, rot, trans])
+            if best_score > interaction_score_threshold and len(interaction_set) < interaction_set_length:
+                interaction_set[0].append([receptor, ligand])
+                interaction_matrix[i, j] = 1
+            if best_score < interaction_score_threshold and len(interaction_set) < interaction_set_length:
+                interaction_set.append([receptor, ligand])
+            interaction_set[1].append(interaction_matrix)
+
+            # fft_score[rot.long(), trans[0].long(), trans[1].long()] -= 100
+            # rot2nd, trans2nd = FFT.extract_transform(fft_score)
+            # best_score_2nd = fft_score[rot2nd.long(), trans2nd[0].long(), trans2nd[1].long()]
+            # FFT_score_list[2].append(best_score_2nd.item())
+
+            FFT_score_list[0].append([i, j])
+            FFT_score_list[1].append(best_score.item())
+            if plotting:
+                pair = Utility.plot_assembly(receptor.cpu(), ligand.cpu(), rot.cpu(), trans.cpu())
+                plt.imshow(pair.transpose())
+                title = 'docking_index_i'+str(i)+'_j'+str(j)+'_score'+str(best_score.item())
+                plt.title(title)
+                plt.savefig('Figs/'+title+'.png')
+                Utility.plot_rotation_energysurface(fft_score, trans, num_angles=360, stream_name='datasetgeneration', plot_count=plot_count)
+
+    if plotting:
+        plt.close()
+        plt.title('Docking Scores by index')
+        plt.ylabel('Scores')
+        plt.scatter(np.arange(0, num_proteins**2), FFT_score_list[1])
+        # plt.show()
+        if swap_quadrants:
+            plt.savefig('Figs/scoredistribution' + protein_pool[:-4] + 'SwapQuadrants.png')
+        else:
+            plt.savefig('Figs/scoredistribution' + protein_pool[:-4] + '.png')
 
     print(FFT_score_list)
+    print('Docking set length', len(docking_set))
+    print('Interaction set length', len(interaction_set))
 
-    # receptor_stack = FFT.make_boundary(receptor)
-    # ligand_stack = FFT.make_boundary(ligand)
-    # fft_score = FFT.dock_global(receptor_stack, ligand_stack)
-    #
-    # FFT.check_FFT_predictions(fft_score, receptor, ligand, gt_txy, gt_rot)
-
-# parser = argparse.ArgumentParser(description='Generate protein docking dataset')
-# parser.add_argument('-train', action='store_const', const=lambda:'train', dest='cmd')
-# parser.add_argument('-test', action='store_const', const=lambda:'test', dest='cmd')
-# parser.add_argument('-debug', action='store_const', const=lambda:'debug', dest='cmd')
-# parser.add_argument('-gen', action='store_const', const=lambda:'gen', dest='act')
-# parser.add_argument('-vis', action='store_const', const=lambda:'vis', dest='act')
-#
-# # parser.add_argument('-IP', action='store_const', const=lambda:'IP', dest='act')
-# # parser.add_argument('-FI', action='store_const', const=lambda:'FI', dest='act')
-#
-#
-# # parser.add_argument('-a00', default=3.0, type=float)
-# # parser.add_argument('-a10', default=-0.3, type=float)
-# # parser.add_argument('-a11', default=2.5, type=float)
-# # parser.add_argument('-score_cutoff', default=-100, type=float)
-# # parser.add_argument('-funnel_gap_cutoff', default=15, type=float)
-# # parser.add_argument('-free_energy_cutoff', default=-100, type=float)
-#
-# parser.add_argument('-a00', default=3.0, type=float)
-# parser.add_argument('-a10', default=-0.3, type=float)
-# parser.add_argument('-a11', default=2.8, type=float)
-# parser.add_argument('-score_cutoff', default=-10, type=float)
-# parser.add_argument('-funnel_gap_cutoff', default=1, type=float)
-# parser.add_argument('-free_energy_cutoff', default=-10, type=float)
-#
-# args = parser.parse_args()
-#
-# inter_dock = InteractionCriteriaGandGap(free_energy_cutoff=args.free_energy_cutoff, funnel_gap_cutoff=args.funnel_gap_cutoff)
-# inter_inter = InteractionCriteriaG(free_energy_cutoff=args.free_energy_cutoff)
-# docker = DockerGPU(a00=args.a00, a10=args.a10, a11=args.a11)
-#
-# if args.cmd is None:
-# 	parser.print_help()
-# 	sys.exit()
-#
-# elif args.cmd() == 'train':
-# 	params = ParamDistribution(
-# 		alpha = [(0.8, 1), (0.9, 9), (0.95, 4)],
-# 		num_points = [(20, 1), (30, 2), (50, 4), (80, 8), (100, 6)]
-# 		)
-#
-# 	savefile = 'DatasetGeneration/Data/protein_pool_big_torchv1p10.pkl'
-# 	if args.act() == 'gen':
-# 		generate_set(500, params, inter_dock, inter_inter, docker, savefile, training_set=True, num_docking_samples=1100, num_inter_proteins=500)
-# 	elif args.act() == 'vis':
-# 		visualize(docker, savefile)
-# 	else:
-# 		print('No action specified')
-#
-# elif args.cmd() == 'test':
-# 	params = ParamDistribution(
-# 		alpha = [(0.85, 4), (0.91, 4), (0.95, 4), (0.98, 4)],
-# 		num_points = [(60, 1), (70, 2), (80, 4), (90, 8), (100, 6), (110, 6), (120, 6)]
-# 		)
-#
-# 	savefile = 'DatasetGeneration/Data/protein_pool_small_torchv1p10.pkl'
-# 	if args.act() == 'gen':
-# 		generate_set(250, params, inter_dock, inter_inter, docker, savefile, training_set=False, num_docking_samples=1100, num_inter_proteins=250)
-# 	elif args.act() == 'vis':
-# 		visualize(docker, savefile)
-# 	else:
-# 		print('No action specified')
-#
-# elif args.cmd() == 'debug':
-# 	params = ParamDistribution(
-# 		alpha = [(0.85, 4), (0.91, 4), (0.95, 4), (0.98, 4)],
-# 		num_points = [(60, 1), (70, 2), (80, 4), (90, 8), (100, 6), (110, 6), (120, 6)]
-# 		)
-#
-# 	savefile = 'DatasetGeneration/Data/protein_pool_debug_torchv1p10.pkl'
-# 	if args.act() == 'gen':
-# 		generate_set(20, params, inter_dock, inter_inter, docker, savefile, training_set=False, num_docking_samples=30, num_inter_proteins=20)
-# 	elif args.act() == 'vis':
-# 		visualize(docker, savefile)
-# 	else:
-# 		print('No action specified')
+    Utility.write_pkl(data=docking_set, fileprefix='docking_set_'+str(len(docking_set))+'examples')
+    Utility.write_pkl(data=interaction_set, fileprefix='docking_set_'+str(len(interaction_set))+'examples')
--- Utility/torchDataLoader.py
+++ Utility/torchDataLoader.py
@@ -41,7 +41,7 @@ class ToyDockingDataset(Dataset):
 		r"""
 		"""
 		receptor, ligand, translation, rotation = self.data[index]
-		return torch.from_numpy(receptor), torch.from_numpy(ligand), torch.tensor(translation), torch.tensor([rotation]), index
+		return receptor, ligand, torch.tensor([rotation]), torch.tensor(translation)
 
 
 	def __len__(self):
@@ -50,61 +50,93 @@ class ToyDockingDataset(Dataset):
 		"""
 		return self.dataset_size
 
-class ToyInteractionDataset(Dataset):
-	r"""
-	"""
-	def __init__(self, path='toy_dataset_1000.pkl', max_size=100, printout=False):
-		r"""
-		"""
-		self.path = path
-		with open(self.path, 'rb') as fin:
-			self.proteins, self.interactome = pkl.load(fin)
+# class ToyDockingDataset(Dataset):
+# 	r"""
+# 	"""
+# 	def __init__(self, path='toy_dataset_1000.pkl', max_size=100):
+# 		r"""
+# 		"""
+# 		self.path = path
+# 		with open(self.path, 'rb') as fin:
+# 			self.data = pkl.load(fin)
+#
+# 		self.data = self.data[:max_size]
+# 		self.dataset_size = len(list(self.data))
+#
+# 		print ("Dataset file: ", self.path)
+# 		print ("Dataset size: ", self.dataset_size)
+#
+# 	def __getitem__(self, index):
+# 		r"""
+# 		"""
+# 		receptor, ligand, translation, rotation = self.data[index]
+# 		return torch.from_numpy(receptor), torch.from_numpy(ligand), torch.tensor(translation), torch.tensor([rotation]), index
+#
+#
+# 	def __len__(self):
+# 		r"""
+# 		Returns length of the dataset
+# 		"""
+# 		return self.dataset_size
 
-		if max_size<len(self.proteins):
-			self.proteins = self.proteins[:max_size]
-			self.interactome = self.interactome[:max_size, :max_size]
-
-		self.interactome = torch.from_numpy(self.interactome).to(dtype=torch.float32)
-		self.num_proteins = len(list(self.proteins))
-
-		self.weights = []
-		self.indexes = []
-		weight = float(self.num_proteins*self.num_proteins)/float(torch.sum(self.interactome).item() + 1e-5)
-		N_pos = 0
-		N_neg = 0
-		for i in range(self.num_proteins):
-			for j in range(self.num_proteins):
-				if i>j: continue
-				self.indexes.append((i,j))
-				if self.interactome[i,j] == 1:
-					self.weights.append(1.0)
-					N_pos += 1
-				else:
-					self.weights.append(1.0/float(weight))
-					N_neg += 1
-
-		self.dataset_size = len(self.indexes)
-		if printout:
-			print ("Dataset file: ", self.path)
-			print ("Dataset size: ", self.dataset_size)
-			print ("Number of proteins: ", len(self.proteins))
-			print ("Positive weight: ", weight, "Number of pos/neg:", N_pos, N_neg)
-
-	def __getitem__(self, index):
-		r"""
-		"""
-		i, j =self.indexes[index]
-		receptor = self.proteins[i]
-		ligand = self.proteins[j]
-		interaction = self.interactome[i,j]
-		return torch.from_numpy(receptor), torch.from_numpy(ligand), torch.tensor([interaction])
-
-
-	def __len__(self):
-		r"""
-		Returns length of the dataset
-		"""
-		return self.dataset_size
+# class ToyInteractionDataset(Dataset):
+# 	r"""
+# 	"""
+# 	def __init__(self, path='toy_dataset_1000.pkl', max_size=100, printout=False):
+# 		r"""
+# 		"""
+# 		self.path = path
+# 		with open(self.path, 'rb') as fin:
+# 			self.proteins, self.interactome = pkl.load(fin)
+# 			print(self.proteins)
+# 			print(self.interactome)
+#
+#
+# 		if max_size<len(self.proteins):
+# 			self.proteins = self.proteins[:max_size]
+# 			self.interactome = self.interactome[:max_size, :max_size]
+#
+# 		self.interactome = torch.from_numpy(self.interactome).to(dtype=torch.float32)
+# 		self.num_proteins = len(list(self.proteins))
+#
+# 		self.weights = []
+# 		self.indexes = []
+# 		weight = float(self.num_proteins*self.num_proteins)/float(torch.sum(self.interactome).item() + 1e-5)
+# 		N_pos = 0
+# 		N_neg = 0
+# 		for i in range(self.num_proteins):
+# 			for j in range(self.num_proteins):
+# 				if i>j: continue
+# 				self.indexes.append((i,j))
+# 				if self.interactome[i,j] == 1:
+# 					self.weights.append(1.0)
+# 					N_pos += 1
+# 				else:
+# 					self.weights.append(1.0/float(weight))
+# 					N_neg += 1
+#
+# 		self.dataset_size = len(self.indexes)
+# 		if printout:
+# 			print ("Dataset file: ", self.path)
+# 			print ("Dataset size: ", self.dataset_size)
+# 			print ("Number of proteins: ", len(self.proteins))
+# 			print ("Positive weight: ", weight, "Number of pos/neg:", N_pos, N_neg)
+#
+# 	def __getitem__(self, index):
+# 		r"""
+# 		"""
+# 		i, j =self.indexes[index]
+# 		receptor = self.proteins[i]
+# 		ligand = self.proteins[j]
+# 		interaction = self.interactome[i,j]
+# 		return torch.from_numpy(receptor), torch.from_numpy(ligand), torch.tensor([interaction])
+#
+#
+# 	def __len__(self):
+# 		r"""
+# 		Returns length of the dataset
+# 		"""
+# 		return self.dataset_size
 
 
 def get_docking_stream(data_path, batch_size = 10, shuffle = False, max_size=1000):
@@ -112,11 +144,13 @@ def get_docking_stream(data_path, batch_size = 10, shuffle = False, max_size=100
 	trainloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=0, shuffle=shuffle, collate_fn=crop_collate)
 	return trainloader
 
-def get_interaction_stream_balanced(data_path, batch_size=10, max_size=1000):
-	dataset = ToyInteractionDataset(path=data_path, max_size=max_size)
-	sampler = BatchSampler(WeightedRandomSampler(weights=dataset.weights, num_samples=len(dataset.weights), replacement=True), batch_size, drop_last=False)
-	trainloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler, num_workers=0, collate_fn=crop_collate, shuffle=False, pin_memory=True)
-	return trainloader
+
+
+# def get_interaction_stream_balanced(data_path, batch_size=10, max_size=1000):
+# 	dataset = ToyInteractionDataset(path=data_path, max_size=max_size)
+# 	sampler = BatchSampler(WeightedRandomSampler(weights=dataset.weights, num_samples=len(dataset.weights), replacement=True), batch_size, drop_last=False)
+# 	trainloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler, num_workers=0, collate_fn=crop_collate, shuffle=False, pin_memory=True)
+# 	return trainloader
 
 # def get_interaction_stream_balanced_singleordering(data_path, sampler=None, printout=False, batch_size=1, max_size=400):
 # 	dataset = ToyInteractionDataset(path=data_path, max_size=max_size, printout=printout)
@@ -134,6 +168,7 @@ def get_interaction_stream(data_path, batch_size = 10, max_size=1000):
 	return trainloader
 
 if __name__=='__main__':
+	pass
 	# stream = get_docking_stream(data_path='DatasetGeneration/docking_data_train.pkl')
 	# for data in stream:
 	# 	receptor, ligand, translation, rotation, index = data
@@ -154,18 +189,18 @@ if __name__=='__main__':
 	# print(ligand.size())
 	# print(interaction)
 
-	stream, sampler = get_interaction_stream_balanced_singleordering(data_path='DatasetGeneration/interaction_data_train.pkl', batch_size=1, max_size=10)
-	# print(stream)
-	first_sampler = []
-	for data in stream:
-		receptor, ligand, interaction = data
-		first_sampler.append(interaction)
-
-	stream, _ = get_interaction_stream_balanced_singleordering(data_path='DatasetGeneration/interaction_data_train.pkl', sampler=sampler, batch_size=1, max_size=10)
-	second_sampler = []
-	for data in stream:
-		receptor, ligand, interaction = data
-		second_sampler.append(interaction)
-
-	print(first_sampler)
-	print(second_sampler)
+	# stream, sampler = get_interaction_stream_balanced_singleordering(data_path='DatasetGeneration/interaction_data_train.pkl', batch_size=1, max_size=10)
+	# # print(stream)
+	# first_sampler = []
+	# for data in stream:
+	# 	receptor, ligand, interaction = data
+	# 	first_sampler.append(interaction)
+	#
+	# stream, _ = get_interaction_stream_balanced_singleordering(data_path='DatasetGeneration/interaction_data_train.pkl', sampler=sampler, batch_size=1, max_size=10)
+	# second_sampler = []
+	# for data in stream:
+	# 	receptor, ligand, interaction = data
+	# 	second_sampler.append(interaction)
+	#
+	# print(first_sampler)
+	# print(second_sampler)
