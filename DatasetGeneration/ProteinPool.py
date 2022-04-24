309242bb5dd2e5ff450e282dd7b16615cc998c04
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-24 19:43:34 -0400

    commented out plotting code, checked param distributions

--- DatasetGeneration/ProteinPool.py
+++ DatasetGeneration/ProteinPool.py
@@ -95,15 +95,15 @@ class ProteinPool:
 		with open(filename, 'wb') as fout:
 			pkl.dump( (self.proteins, self.params, self.interactions), fout)
 
-	def get_interactions(self, docker):
-		interactome = Interactome(docker, proteins = self.proteins)
-		for i, j, interaction in interactome.all():
-			funnels, complexes = interaction.find_funnels(num_funnels=2)
-			if len(funnels) == 2:
-				funnel_gap = torch.min(funnels[1][1]).item() - torch.min(funnels[0][1]).item()
-			else:
-				funnel_gap = 0.0
-			self.interactions[(i,j)] = (interaction.est_binding(), interaction.min_score, funnel_gap)
+	# def get_interactions(self, docker):
+	# 	interactome = Interactome(docker, proteins = self.proteins)
+	# 	for i, j, interaction in interactome.all():
+	# 		funnels, complexes = interaction.find_funnels(num_funnels=2)
+	# 		if len(funnels) == 2:
+	# 			funnel_gap = torch.min(funnels[1][1]).item() - torch.min(funnels[0][1]).item()
+	# 		else:
+	# 			funnel_gap = 0.0
+	# 		self.interactions[(i,j)] = (interaction.est_binding(), interaction.min_score, funnel_gap)
 
 	def create_complexes(self, params):
 		new_proteins = []
@@ -121,33 +121,33 @@ class ProteinPool:
 		new_pool.params = new_params
 		return new_pool
 	
-	def extract_docking_dataset(self, docker, interaction_criteria, max_num_samples=1000):
-		pairs = [key for key, inter in self.interactions.items() if interaction_criteria(inter)]
-		dataset = []
-		for i, j in tqdm(pairs, total=max_num_samples):
-			receptor, ligand = Protein(self.proteins[i]), Protein(self.proteins[j])
-			scores = docker.dock_global(receptor, ligand)
-			min_score, cplx, ind = docker.get_conformation(scores, receptor, ligand)
-			dataset.append((cplx.receptor.bulk.copy(), cplx.ligand.bulk.copy(), cplx.translation.copy(), cplx.rotation))
-			if len(dataset) == max_num_samples:
-				break
-		return dataset
+	# def extract_docking_dataset(self, docker, interaction_criteria, max_num_samples=1000):
+	# 	pairs = [key for key, inter in self.interactions.items() if interaction_criteria(inter)]
+	# 	dataset = []
+	# 	for i, j in tqdm(pairs, total=max_num_samples):
+	# 		receptor, ligand = Protein(self.proteins[i]), Protein(self.proteins[j])
+	# 		scores = docker.dock_global(receptor, ligand)
+	# 		min_score, cplx, ind = docker.get_conformation(scores, receptor, ligand)
+	# 		dataset.append((cplx.receptor.bulk.copy(), cplx.ligand.bulk.copy(), cplx.translation.copy(), cplx.rotation))
+	# 		if len(dataset) == max_num_samples:
+	# 			break
+	# 	return dataset
 
-	def extract_interactome_dataset(self, interaction_criteria, ind_range=(0, 900)):
-		proteins_sel = [protein for n, protein in enumerate(self.proteins) if ((n>=ind_range[0]) and (n<ind_range[1]))]
-		
-		N = ind_range[1] - ind_range[0]
-		mat = np.zeros((N,N))
-		for (i,j), inter in self.interactions.items():
-			if not(i >= ind_range[0] and i<ind_range[1]):
-				continue
-			if not(j >= ind_range[0] and j<ind_range[1]):
-				continue
-			if interaction_criteria(inter):
-				mat[i-ind_range[0], j-ind_range[0]] = 1.0
-				mat[j-ind_range[0], i-ind_range[0]] = 1.0
-		
-		return proteins_sel.copy(), mat.copy()
+	# def extract_interactome_dataset(self, interaction_criteria, ind_range=(0, 900)):
+	# 	proteins_sel = [protein for n, protein in enumerate(self.proteins) if ((n>=ind_range[0]) and (n<ind_range[1]))]
+	#
+	# 	N = ind_range[1] - ind_range[0]
+	# 	mat = np.zeros((N,N))
+	# 	for (i,j), inter in self.interactions.items():
+	# 		if not(i >= ind_range[0] and i<ind_range[1]):
+	# 			continue
+	# 		if not(j >= ind_range[0] and j<ind_range[1]):
+	# 			continue
+	# 		if interaction_criteria(inter):
+	# 			mat[i-ind_range[0], j-ind_range[0]] = 1.0
+	# 			mat[j-ind_range[0], i-ind_range[0]] = 1.0
+	#
+	# 	return proteins_sel.copy(), mat.copy()
 
 	def plot_interaction_dist(self, perc=80):
 		assert not(self.interactions is None)
@@ -263,51 +263,51 @@ class ProteinPool:
 		else:
 			plt.savefig(filename)
 
-	def plot_sample_funnels(self, docker, filename='funnels.png', range=[(-100, -80), (-70, -40), (-32, -20)], titles=['A','B','C']):
-		import matplotlib.font_manager as font_manager
-		# font_manager.fontManager.addfont('/home/lupoglaz/.fonts/Helvetica.ttf')
-		font_manager.fontManager.addfont('Helvetica.ttf')
-		rcParams['font.family'] = 'Helvetica'
-		font = {'weight': 'normal',
-				'size': 18,
-				}
-
-		fig, axs = plt.subplots(1, 3, sharey=True, figsize=(18,6))
-		fig.subplots_adjust(wspace=0)
-		num_samples = len(range)
-		sample_num = 0
-		for (i, j), (G, score, gap) in self.interactions.items():
-			if not(score>range[sample_num][0] and score<=range[sample_num][1]):
-				continue
-			rec, lig = Protein(self.proteins[i]), Protein(self.proteins[j])
-			scores = docker.dock_global(rec, lig)
-			inter = Interaction(docker, scores, rec, lig)
-			
-			if sample_num ==0:
-				inter.plot_funnels(ax=axs[sample_num], im_offset=(70,25))
-			else:
-				inter.plot_funnels(ax=axs[sample_num], im_offset=(60, -75))
-			
-			axs[sample_num].set_title(titles[sample_num], fontsize=24)
-			axs[sample_num].set_xlabel('RMSD', fontsize=20)
-			# axs[sample_num].legend(prop=font)
-			for label in axs[sample_num].get_xticklabels():
-				label.set_fontproperties(font)
-
-			for label in axs[sample_num].get_yticklabels():
-				label.set_fontproperties(font)
-
-			sample_num += 1
-			if sample_num == num_samples:
-				break
-			
-
-		axs[0].set_ylabel('Energy', fontsize=20)
-		plt.tight_layout()
-		if filename is None:
-			plt.show()
-		else:
-			plt.savefig(filename)
+	# def plot_sample_funnels(self, docker, filename='funnels.png', range=[(-100, -80), (-70, -40), (-32, -20)], titles=['A','B','C']):
+	# 	import matplotlib.font_manager as font_manager
+	# 	# font_manager.fontManager.addfont('/home/lupoglaz/.fonts/Helvetica.ttf')
+	# 	font_manager.fontManager.addfont('Helvetica.ttf')
+	# 	rcParams['font.family'] = 'Helvetica'
+	# 	font = {'weight': 'normal',
+	# 			'size': 18,
+	# 			}
+	#
+	# 	fig, axs = plt.subplots(1, 3, sharey=True, figsize=(18,6))
+	# 	fig.subplots_adjust(wspace=0)
+	# 	num_samples = len(range)
+	# 	sample_num = 0
+	# 	for (i, j), (G, score, gap) in self.interactions.items():
+	# 		if not(score>range[sample_num][0] and score<=range[sample_num][1]):
+	# 			continue
+	# 		rec, lig = Protein(self.proteins[i]), Protein(self.proteins[j])
+	# 		scores = docker.dock_global(rec, lig)
+	# 		inter = Interaction(docker, scores, rec, lig)
+	#
+	# 		if sample_num ==0:
+	# 			inter.plot_funnels(ax=axs[sample_num], im_offset=(70,25))
+	# 		else:
+	# 			inter.plot_funnels(ax=axs[sample_num], im_offset=(60, -75))
+	#
+	# 		axs[sample_num].set_title(titles[sample_num], fontsize=24)
+	# 		axs[sample_num].set_xlabel('RMSD', fontsize=20)
+	# 		# axs[sample_num].legend(prop=font)
+	# 		for label in axs[sample_num].get_xticklabels():
+	# 			label.set_fontproperties(font)
+	#
+	# 		for label in axs[sample_num].get_yticklabels():
+	# 			label.set_fontproperties(font)
+	#
+	# 		sample_num += 1
+	# 		if sample_num == num_samples:
+	# 			break
+	#
+	#
+	# 	axs[0].set_ylabel('Energy', fontsize=20)
+	# 	plt.tight_layout()
+	# 	if filename is None:
+	# 		plt.show()
+	# 	else:
+	# 		plt.savefig(filename)
 
 	def plot_params(self, output_name='stats.png'):
 		from mpl_toolkits.axes_grid1 import make_axes_locatable
