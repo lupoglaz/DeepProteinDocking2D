819d311caead14f19c68fbe201c35ce0f523b2ee
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-28 14:13:15 -0400

    commented out unused codel

--- DatasetGeneration/Complex.py
+++ DatasetGeneration/Complex.py
@@ -4,6 +4,7 @@ import seaborn as sea
 sea.set_style("whitegrid")
 from random import uniform
 
+
 def _pick_translation(receptor, ligand, threshold):
 	rec_center = np.array([receptor.shape[0]/2, receptor.shape[1]/2])
 	lig_center = np.array([ligand.shape[0]/2, ligand.shape[1]/2])
@@ -20,7 +21,8 @@ def _pick_translation(receptor, ligand, threshold):
 			return t, overlap
 
 	return None, None
-	
+
+
 def _superpose_volumes(v1, v2, translation):
 	def mabs(dx, dy):		
 		return np.abs(dx), np.abs(dy)
@@ -61,6 +63,7 @@ def _superpose_volumes(v1, v2, translation):
 	
 	return v1_crop, v2_crop
 
+
 class Complex:
 	def __init__(self, receptor, ligand, rotation, translation):
 		self.receptor = receptor
@@ -88,55 +91,55 @@ class Complex:
 		
 		return cls(receptor, ligand, rotation, translation)
 
-	def score(self, boundary_size=3, a00=1.0, a11=-1.0, a10=-1.0):
-		if self.receptor.boundary is None:
-			self.receptor.make_boundary(boundary_size=boundary_size)
-		if self.ligand.boundary is None:
-			self.ligand.make_boundary(boundary_size=boundary_size)
-		trligand = self.ligand.rotate(self.rotation).translate(self.translation)
-		t00 = np.sum(self.receptor.bulk * trligand.bulk)
-		t11 = np.sum(self.receptor.boundary * trligand.boundary)
-		t10 = np.sum(self.receptor.bulk * trligand.boundary) + np.sum(self.receptor.boundary * trligand.bulk)
-		score = a11*t11 + a10*t10*0.5 + a00*t00
-		return score
-	
-	def get_canvas(self, cell_size):
-		rligand = self.ligand.rotate(self.rotation)
-		trligand = rligand.translate(self.translation)
-		dx, dy = int(self.translation[0]), int(self.translation[1])
-		prot_size = self.receptor.bulk.shape[0]
-
-		rec_x_start = int(cell_size/2) - int(prot_size/2) - int(dx/2)
-		rec_x_end = int(cell_size/2) + int(prot_size/2) - int(dx/2)
-		rec_y_start = int(cell_size/2) - int(prot_size/2) - int(dy/2)
-		rec_y_end = int(cell_size/2) + int(prot_size/2) - int(dy/2)
-		lig_x_start = int(cell_size/2) - int(prot_size/2) + int(dx/2)
-		lig_x_end = int(cell_size/2) + int(prot_size/2) + int(dx/2)
-		lig_y_start = int(cell_size/2) - int(prot_size/2) + int(dy/2)
-		lig_y_end = int(cell_size/2) + int(prot_size/2) + int(dy/2)
-
-		canvas = np.zeros( (cell_size, cell_size) )
-		canvas[rec_x_start:rec_x_end, rec_y_start:rec_y_end] = self.receptor.bulk
-		canvas[lig_x_start:lig_x_end, lig_y_start:lig_y_end] += 2*rligand.bulk
-		canvas[canvas>2.0] = 2.0
-		return canvas
-
-	def plot(self):
-		rligand = self.ligand.rotate(self.rotation)
-		trligand = rligand.translate(self.translation)
-		dx, dy = int(self.translation[0]), int(self.translation[1])
-		# print('Translation=', dx, dy, 'Rotation:', self.rotation)
-		# print('Score=', self.score())
-		field = np.zeros( (200, 200) )
-		field[75:125, 75:125] = self.receptor.bulk
-		field[  100 + dx - 25: 100 + dx + 25, 
-				100 + dy - 25: 100 + dy + 25] += 2*rligand.bulk
-		# field[  75: 125, 
-		#  		75: 125] += 2*trligand.bulk
-		field[150:200,150:200]  = self.receptor.bulk
-		field[150:200,100:150]  = self.ligand.bulk
-		plt.imshow(field)
-		# plt.show()
+	# def score(self, boundary_size=3, a00=1.0, a11=-1.0, a10=-1.0):
+	# 	if self.receptor.boundary is None:
+	# 		self.receptor.make_boundary(boundary_size=boundary_size)
+	# 	if self.ligand.boundary is None:
+	# 		self.ligand.make_boundary(boundary_size=boundary_size)
+	# 	trligand = self.ligand.rotate(self.rotation).translate(self.translation)
+	# 	t00 = np.sum(self.receptor.bulk * trligand.bulk)
+	# 	t11 = np.sum(self.receptor.boundary * trligand.boundary)
+	# 	t10 = np.sum(self.receptor.bulk * trligand.boundary) + np.sum(self.receptor.boundary * trligand.bulk)
+	# 	score = a11*t11 + a10*t10*0.5 + a00*t00
+	# 	return score
+	#
+	# def get_canvas(self, cell_size):
+	# 	rligand = self.ligand.rotate(self.rotation)
+	# 	trligand = rligand.translate(self.translation)
+	# 	dx, dy = int(self.translation[0]), int(self.translation[1])
+	# 	prot_size = self.receptor.bulk.shape[0]
+	#
+	# 	rec_x_start = int(cell_size/2) - int(prot_size/2) - int(dx/2)
+	# 	rec_x_end = int(cell_size/2) + int(prot_size/2) - int(dx/2)
+	# 	rec_y_start = int(cell_size/2) - int(prot_size/2) - int(dy/2)
+	# 	rec_y_end = int(cell_size/2) + int(prot_size/2) - int(dy/2)
+	# 	lig_x_start = int(cell_size/2) - int(prot_size/2) + int(dx/2)
+	# 	lig_x_end = int(cell_size/2) + int(prot_size/2) + int(dx/2)
+	# 	lig_y_start = int(cell_size/2) - int(prot_size/2) + int(dy/2)
+	# 	lig_y_end = int(cell_size/2) + int(prot_size/2) + int(dy/2)
+	#
+	# 	canvas = np.zeros( (cell_size, cell_size) )
+	# 	canvas[rec_x_start:rec_x_end, rec_y_start:rec_y_end] = self.receptor.bulk
+	# 	canvas[lig_x_start:lig_x_end, lig_y_start:lig_y_end] += 2*rligand.bulk
+	# 	canvas[canvas>2.0] = 2.0
+	# 	return canvas
+	#
+	# def plot(self):
+	# 	rligand = self.ligand.rotate(self.rotation)
+	# 	trligand = rligand.translate(self.translation)
+	# 	dx, dy = int(self.translation[0]), int(self.translation[1])
+	# 	# print('Translation=', dx, dy, 'Rotation:', self.rotation)
+	# 	# print('Score=', self.score())
+	# 	field = np.zeros( (200, 200) )
+	# 	field[75:125, 75:125] = self.receptor.bulk
+	# 	field[  100 + dx - 25: 100 + dx + 25,
+	# 			100 + dy - 25: 100 + dy + 25] += 2*rligand.bulk
+	# 	# field[  75: 125,
+	# 	#  		75: 125] += 2*trligand.bulk
+	# 	field[150:200,150:200]  = self.receptor.bulk
+	# 	field[150:200,100:150]  = self.ligand.bulk
+	# 	plt.imshow(field)
+	# 	# plt.show()
 
 # if __name__ == '__main__':
 # 	Complex = Complex(receptor=, ligand=, )
