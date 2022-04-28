120c35046904d5e1b7bcc3af3643f8b12a8d1883
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-28 01:16:01 -0400

    plotting works, shared axis next

--- Plotting/plot_shape_params.py
+++ Plotting/plot_shape_params.py
@@ -6,7 +6,29 @@ sea.set_style("whitegrid")
 from matplotlib import gridspec
 
 
-def plot_shape_and_params(protein_pool):
+def plot_fractions(counts, dataname):
+    unique = np.unique(counts)
+    bins = len(unique)
+    y, _, _ = plt.hist(counts, bins=bins)  # just calling this as a shortcut to get counts
+    plt.close()
+    fracs = y / sum(y)
+    print('\n'+dataname+': ')
+    print('unique values', unique)
+    print('counts', y)
+    print('fractions', fracs)
+
+    barwidth = (unique[-1] - unique[-2])/2
+    plt.bar(unique, fracs, width=barwidth)
+    plt.xticks(unique)
+    plt.ylabel('fraction of dataset')
+    plt.xlabel(dataname)
+    # plt.savefig(dataname+'_barplots.png')
+    plt.close()
+
+    return unique, fracs, barwidth
+
+
+def plot_shape_and_params(protein_pool, datasetname):
     protein_pool_prefix = protein_pool[:-4]
     data = UtilityFuncs().read_pkl(protein_pool_prefix)
     protein_shapes = data[0]
@@ -45,7 +67,6 @@ def plot_shape_and_params(protein_pool):
     ax1.grid(False)
     ax1.set_axis_off()
 
-
     ax2.barh(alpha_unique, alpha_fracs, alpha_barwidth)
     ax2.set_yticks(alpha_unique)
     ax2.grid(False)
@@ -56,7 +77,7 @@ def plot_shape_and_params(protein_pool):
     # ax1.sharex(ax0)
     # ax1.sharey(ax2)
     plt.tight_layout()
-    plt.savefig('combined_shapes_params')
+    plt.savefig(datasetname+str(len(protein_shapes))+'pool_combined_shapes_params')
     plt.show()
 
     # gs = gridspec.GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1])
@@ -81,28 +102,6 @@ def plot_shape_and_params(protein_pool):
     # plt.show()
 
 
-def plot_fractions(counts, dataname):
-    unique = np.unique(counts)
-    bins = len(unique)
-    y, _, _ = plt.hist(counts, bins=bins)  # just calling this as a shortcut to get counts
-    plt.close()
-    fracs = y / sum(y)
-    print('\n'+dataname+': ')
-    print('unique values', unique)
-    print('counts', y)
-    print('fractions', fracs)
-
-    barwidth = (unique[-1] - unique[-2])/2
-    barplot = plt.bar(unique, fracs, width=barwidth)
-    plt.xticks(unique)
-    plt.ylabel('fraction of dataset')
-    plt.xlabel(dataname)
-    plt.savefig(dataname+'_barplots.png')
-    plt.close()
-
-    return unique, fracs, barwidth
-
-
 def plot_shape_distributions(protein_shapes, alpha_unique, numpoints_unique, params_list, protein_pool_prefix):
     alphas_list = list(alpha_unique)
     numpoints_list = list(numpoints_unique)
@@ -110,29 +109,43 @@ def plot_shape_distributions(protein_shapes, alpha_unique, numpoints_unique, par
     combination_list = [[i,j] for i in alphas_list for j in numpoints_list]
 
     indices = []
-    for i in range(len(params_list)):
-        if params_list[i] in combination_list:
-            indices.append(i)
-            combination_list.remove(params_list[i])
+    combination_list_copy = combination_list[:]
+
+    found_list = []
+    for i in combination_list_copy:
+        for j in range(len(params_list)):
+            if i == params_list[j] and i not in found_list:
+                # print(i, params_list[j])
+                found_list.append(i)
+                indices.append(j)
+
+    # print(combination_list)
+    # print(combination_list_copy)
 
     num_rows = len(alphas_list)
     num_cols = len(numpoints_list)
+
+    print(indices)
 
     plot_ranges = []
     for i in range(num_rows):
         cur_range = [*range(i*num_cols, (i+1)*num_cols)]
         plot_ranges.append(cur_range)
 
+    # print(plot_ranges)
+
     plot_rows = []
     for i in plot_ranges:
-        cur_row = np.array(protein_shapes)[i]
+        cur_indices = np.array(indices)[i]
+        cur_row = np.array(protein_shapes)[cur_indices]
         plot_rows.append(np.hstack(cur_row))
 
-    shapes_plot = np.vstack(plot_rows)
+
+    shapes_plot = np.vstack(plot_rows[::-1])
 
     plt.imshow(shapes_plot)
     plt.grid(False)
-    plt.savefig('shape_distribution')
+    # plt.savefig('shape_distribution')
     plt.close()
 
     return shapes_plot, num_rows, num_cols
@@ -141,7 +154,13 @@ def plot_shape_distributions(protein_shapes, alpha_unique, numpoints_unique, par
 if __name__ == "__main__":
 
     data_path = '../DatasetGeneration/'
-    trainpool_num_proteins = 400
-    trainvalidset_protein_pool = data_path+'trainvalidset_protein_pool' + str(trainpool_num_proteins) + '.pkl'
 
-    plot_shape_and_params(trainvalidset_protein_pool)
+    num_proteins = 400
+    trainvalidset_protein_pool = data_path+'trainvalidset_protein_pool' + str(num_proteins) + '.pkl'
+
+    plot_shape_and_params(trainvalidset_protein_pool, 'trainset')
+
+    num_proteins = 200
+    testset_protein_pool = data_path+'testset_protein_pool' + str(num_proteins) + '.pkl'
+
+    plot_shape_and_params(testset_protein_pool, 'testset')
