9af179d22b637617a5042fc4fa131c07c7784c2c
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-04-29 18:20:18 -0400

    adding missing param combos when needed and plotting bar spacing matches imshow fig

--- Plotting/plot_shape_params.py
+++ Plotting/plot_shape_params.py
@@ -1,43 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from DeepProteinDocking2D.Utility.utility_functions import UtilityFuncs
+from DeepProteinDocking2D.DatasetGeneration.ProteinPool import ProteinPool, Protein
 import seaborn as sea
 sea.set_style("whitegrid")
-from matplotlib import gridspec
+from collections import Counter
+from tqdm import tqdm
 
 
-def plot_fractions(counts, dataname):
-    unique = np.unique(counts)
-    bins = len(unique)
-    y, _, _ = plt.hist(counts, bins=bins)  # just calling this as a shortcut to get counts
-    plt.close()
-    fracs = y / sum(y)
-    print('\n'+dataname+': ')
-    print('unique values', unique)
-    print('counts', y)
-    print('fractions', fracs)
-
-    barwidth = (unique[-1] - unique[-2])/2
-    plt.bar(unique, fracs, width=barwidth)
-    plt.xticks(unique)
-    plt.ylabel('fraction of dataset')
-    plt.xlabel(dataname)
-    # plt.savefig(dataname+'_barplots.png')
-    plt.close()
-
-    return unique, fracs, barwidth
+def get_counts(counts):
+    counter = Counter(counts)
+    unique = list(sorted(counter.keys()))
+    counts = list(sorted(counter.values()))
+    return unique, counts
 
 
-def plot_shape_and_params(protein_pool, datasetname):
-    protein_pool_prefix = protein_pool[:-4]
-    data = UtilityFuncs().read_pkl(protein_pool_prefix)
-    protein_shapes = data[0]
-    shape_params = data[1]
-
+def get_dict_counts(shape_params):
     alpha_counts = []
     numpoints_counts = []
     params_list = []
-
     for dict in shape_params:
         items = list(dict.items())
         alpha = items[0][1]
@@ -46,19 +26,132 @@ def plot_shape_and_params(protein_pool, datasetname):
         numpoints_counts.append(numpoints)
         params_list.append([alpha, numpoints])
 
-    alpha_unique, alpha_fracs, alpha_barwidth = plot_fractions(counts=alpha_counts, dataname='alphas')
-    numpoints_unique, numpoints_fracs, numpoints_barwidth = plot_fractions(counts=numpoints_counts, dataname='number of points')
+    return alpha_counts, numpoints_counts, params_list
 
-    shapes_plot, num_rows, num_cols = plot_shape_distributions(protein_shapes, alpha_unique, numpoints_unique, params_list, protein_pool_prefix)
 
-    fig, ax = plt.subplots(2,2, gridspec_kw={'width_ratios': [2, 1],'height_ratios': [1, 1]})
+def get_unique_fracs(counts):
+    unique, counts = get_counts(counts)
+    fracs = [i/sum(counts) for i in counts]
+    print('unique values',unique)
+    print('counts', counts)
+    print('fractions', fracs)
+    barwidth = (unique[-1] - unique[0])/len(unique)
+
+    return unique, fracs, barwidth
+
+
+def check_missing_examples(combination_list, found_list, protein_shapes, params_list):
+    missing_list = []
+    missing_indices = []
+    for i in range(len(combination_list)):
+        if combination_list[i] not in found_list:
+            missing_list.append(combination_list[i])
+            missing_indices.append(i)
+
+    print('\tMissing examples of [alpha, number of points]:', missing_list)
+    print('Regenerating missing examples...')
+    for i in tqdm(range(len(missing_list))):
+        alpha = missing_list[i][0]
+        num_points = missing_list[i][1]
+        prot = Protein.generateConcave(size=50, alpha=alpha, num_points=num_points)
+        protein_shapes.append(prot.bulk)
+        params_list.append([alpha, num_points])
+
+    indices = []
+    found_list = []
+    for i in combination_list:
+        for j in range(len(params_list)):
+            if i == params_list[j] and i not in found_list:
+                found_list.append(i)
+                indices.append(j)
+
+    print('missing indices', missing_indices)
+    print('missing_list', missing_list)
+    print('foundlist', found_list)
+
+    return indices
+
+
+def get_shape_distributions(data, alpha_counts, numpoints_counts, params_list, debug=False):
+    protein_shapes = data.proteins
+    shape_params = data.params
+
+    alphas_unique, alphas_fracs, alphas_barwidth = get_unique_fracs(alpha_counts)
+    numpoints_unique, numpoints_fracs, numpoints_barwidth = get_unique_fracs(numpoints_counts)
+
+    combination_list = [[i,j] for i in alphas_unique for j in numpoints_unique]
+
+    indices = []
+    found_list = []
+    for i in combination_list:
+        for j in range(len(params_list)):
+            if i == params_list[j] and i not in found_list:
+                found_list.append(i)
+                indices.append(j)
+
+    if len(found_list) < len(combination_list):
+        print('ERROR: Missing combination of alpha and number of points in original dataset loaded')
+        print('\tIncrease probabilities to increase parameter combination encounter frequencies')
+        indices = check_missing_examples(combination_list, found_list, protein_shapes, params_list)
+
+    num_rows = len(alphas_unique)
+    num_cols = len(numpoints_unique)
+
+    plot_ranges = []
+    for i in range(num_rows):
+        cur_range = [*range(i*num_cols, (i+1)*num_cols)]
+        plot_ranges.append(cur_range)
+
+    plot_rows = []
+    for i in plot_ranges[::-1]:
+        cur_indices = np.array(indices)[i]
+        cur_row = np.array(protein_shapes)[cur_indices]
+        plot_rows.append(np.hstack(cur_row))
+
+    shapes_plot = np.vstack(plot_rows)
+
+    if debug:
+        for i in indices:
+            print('params', shape_params[i])
+            plt.imshow(protein_shapes[i])
+            plt.show()
+
+    alphas_packed = alphas_unique, alphas_fracs, alphas_barwidth
+    numpoints_packed = numpoints_unique, numpoints_fracs, numpoints_barwidth
+
+    return shapes_plot, alphas_packed, numpoints_packed
+
+
+def plot_shape_and_params(protein_pool, dataset_name):
+    data = ProteinPool.load(protein_pool)
+
+    protein_shapes = data.proteins
+    shape_params = data.params
+
+    alpha_counts, numpoints_counts, params_list = get_dict_counts(shape_params)
+
+    shapes_plot, alphas_packed, numpoints_packed = get_shape_distributions(data, alpha_counts, numpoints_counts, params_list)
+
+    alphas_unique,  alphas_fracs, alphas_barwidth = alphas_packed
+    numpoints_unique, numpoints_fracs, numpoints_barwidth = numpoints_packed
+
+    # num_rows = len(alphas_unique)
+    # num_cols = len(numpoints_unique)
+
+    print('unique values: ', alphas_unique, numpoints_unique)
+
+    gs_kw = dict(width_ratios=[1, 1], height_ratios=[1, 2])
+    fig, ax = plt.subplots(2, 2, gridspec_kw=gs_kw)
     ax0 = ax[0,0]
     axoff = ax[0,1]
     axoff.set_axis_off()
     ax1 = ax[1,0]
     ax2 = ax[1,1]
 
-    ax0.bar(numpoints_unique, numpoints_fracs, numpoints_barwidth)
+    numpoints_unique_strs = [str(i) for i in numpoints_unique]
+    alphas_unique_strs = [str(i) for i in alphas_unique]
+
+    ax0.bar(numpoints_unique_strs, numpoints_fracs)
     ax0.grid(False)
     plt.setp(ax0, ylabel='fraction')
     plt.setp(ax0, xlabel='number of points')
@@ -67,101 +160,27 @@ def plot_shape_and_params(protein_pool, datasetname):
     ax1.grid(False)
     ax1.set_axis_off()
 
-    ax2.barh(alpha_unique, alpha_fracs, alpha_barwidth)
-    ax2.set_yticks(alpha_unique)
+    ax2.barh(alphas_unique_strs, alphas_fracs)
     ax2.grid(False)
     plt.setp(ax2, xlabel='fraction')
     plt.setp(ax2, ylabel='alphas')
 
-    # ax1.get_shared_x_axes().join(ax0, ax2)
-    # ax1.sharex(ax0)
-    # ax1.sharey(ax2)
     plt.tight_layout()
-    # plt.subplot_tool()
 
-    plt.savefig(datasetname+str(len(protein_shapes))+'pool_combined_shapes_params')
+    plt.savefig(dataset_name + str(len(data.proteins)) + 'pool_combined_shapes_params')
     plt.show()
-
-    # gs = gridspec.GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1])
-    # ax0 = plt.subplot(gs[0])
-    # ax1 = plt.subplot(gs[2])
-    # ax2 = plt.subplot(gs[3])
-    #
-    # ax0.bar(numpoints_unique, numpoints_fracs, numpoints_barwidth)
-    # ax0.grid(False)
-    # xaxis = plt.setp(ax0.get_xticklabels(), visible=False)
-    #
-    # ax1.imshow(shapes_plot)
-    # ax1.set_axis_off()
-    # ax1.grid(False)
-    #
-    # ax2.barh(alpha_unique, alpha_fracs, alpha_barwidth)
-    # ax2.set_yticks(alpha_unique)
-    # ax2.grid(False)
-    #
-    # plt.tight_layout()
-    # plt.savefig('combined_shapes_params')
-    # plt.show()
-
-
-def plot_shape_distributions(protein_shapes, alpha_unique, numpoints_unique, params_list, protein_pool_prefix):
-    alphas_list = list(alpha_unique)
-    numpoints_list = list(numpoints_unique)
-
-    combination_list = [[i,j] for i in alphas_list for j in numpoints_list]
-
-    indices = []
-    combination_list_copy = combination_list[:]
-
-    found_list = []
-    for i in combination_list_copy:
-        for j in range(len(params_list)):
-            if i == params_list[j] and i not in found_list:
-                # print(i, params_list[j])
-                found_list.append(i)
-                indices.append(j)
-
-    # print(combination_list)
-    # print(combination_list_copy)
-
-    num_rows = len(alphas_list)
-    num_cols = len(numpoints_list)
-
-    print(indices)
-
-    plot_ranges = []
-    for i in range(num_rows):
-        cur_range = [*range(i*num_cols, (i+1)*num_cols)]
-        plot_ranges.append(cur_range)
-
-    # print(plot_ranges)
-
-    plot_rows = []
-    for i in plot_ranges:
-        cur_indices = np.array(indices)[i]
-        cur_row = np.array(protein_shapes)[cur_indices]
-        plot_rows.append(np.hstack(cur_row))
-
-    shapes_plot = np.vstack(plot_rows[::-1])
-
-    plt.imshow(shapes_plot)
-    plt.grid(False)
-    # plt.savefig('shape_distribution')
-    plt.close()
-
-    return shapes_plot, num_rows, num_cols
 
 
 if __name__ == "__main__":
 
     data_path = '../DatasetGeneration/'
 
-    num_proteins = 200
+    num_proteins = 50
     trainvalidset_protein_pool = data_path+'trainvalidset_protein_pool' + str(num_proteins) + '.pkl'
 
     plot_shape_and_params(trainvalidset_protein_pool, 'trainset')
 
-    num_proteins = 100
+    num_proteins = 50
     testset_protein_pool = data_path+'testset_protein_pool' + str(num_proteins) + '.pkl'
 
     plot_shape_and_params(testset_protein_pool, 'testset')
