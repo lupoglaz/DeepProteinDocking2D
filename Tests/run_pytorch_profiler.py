7344b01c7df57b3569ce86b26e6cbb261841b4b3
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-05-07 16:37:15 -0400

    better profiler print outs

--- Tests/run_pytorch_profiler.py
+++ Tests/run_pytorch_profiler.py
@@ -3,21 +3,25 @@ import torch.autograd.profiler as profiler
 
 from DeepProteinDocking2D.Models.BruteForce.train_bruteforce_docking import BruteForceDockingTrainer, Docking, get_docking_stream
 
-#### RUN PROFILER
-# warm-up ^^^
-
 experiment = 'BF_IP_FINAL_DATASET_400pool_1000ex_30ep'
 
 ######################
-train_epochs = 30
+trainset = '../Datasets/docking_train_400pool'
+
+train_epochs = 1
 lr = 10 ** -4
 model = Docking().to(device=0)
 optimizer = optim.Adam(model.parameters(), lr=lr)
-trainset = '../../Datasets/docking_train_400pool'
 train_stream = get_docking_stream(trainset+'.pkl', max_size=10)
+
+### warm-up
+BruteForceDockingTrainer(model, optimizer, experiment).run_trainer(
+    train_epochs=train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
+
+#### run profiler
 with profiler.profile(with_stack=True, profile_memory=True) as prof:
     BruteForceDockingTrainer(model, optimizer, experiment).run_trainer(
         train_epochs=train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
 
-# print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=5))
-print(prof.key_averages().table())
+print(prof.key_averages().table(sort_by='self_cpu_time_total'))
+print(prof.total_average())
